967c00638fcb1de2800cec0d8722e3f190cb8ed4
Wesley Tsang <tsangwe@oocl.com>
2020-04-16 15:05:15 +0800

    [Wesley] Pass test should_return_employees_when_get_employees_in_company

--- src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.google.gson.Gson;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.CompanyService;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,28 +38,35 @@ public class CompanyControllerTest {
     private CompanyController companyController;
     @Autowired
     private CompanyService companyService;
+    private Gson gson;
     private static final String DEFAULT_PATH_PREFIX = "/api/company";
+    private Company company1;
+    private Company company2;
+    private Company company3;
 
     @Before
     public void setup() {
+        gson = new Gson();
         StandaloneMockMvcBuilder standaloneMockMvcBuilder
                 = MockMvcBuilders.standaloneSetup(companyController);
         RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
 
-        companyService.addCompany(new Company(1, "Big Company", 3,
+        company1 = new Company(1, "Big Company", 3,
                 Stream.of(new Employee(1, "Wesley", 10, "Male", 1),
                         new Employee(2, "Andy", 11, "Male", 5),
                         new Employee(3, "Angle", 9, "Female", 5000))
-                .collect(Collectors.toList())));
-
-        companyService.addCompany(new Company(2, "Small Company", 2,
+                        .collect(Collectors.toList()));
+        company2 = new Company(2, "Small Company", 2,
                 Stream.of(new Employee(1, "Suki", 15, "Female", 1000),
                         new Employee(2, "Junie", 16, "Female", 5000))
-                        .collect(Collectors.toList())));
-
-        companyService.addCompany(new Company(3, "Small Potato", 1,
+                        .collect(Collectors.toList()));
+        company3 = new Company(3, "Small Potato", 1,
                 Stream.of(new Employee(1, "Tony", 30, "Female", 0))
-                        .collect(Collectors.toList())));
+                        .collect(Collectors.toList()));
+
+        companyService.addCompany(company1);
+        companyService.addCompany(company2);
+        companyService.addCompany(company3);
     }
 
     @Test
@@ -72,7 +81,11 @@ public class CompanyControllerTest {
                 return super.getType();
             }
         });
-        Assert.assertEquals(3, companies.size());
+        List<Company> expectedOutput = new ArrayList<>();
+        expectedOutput.add(company1);
+        expectedOutput.add(company2);
+        expectedOutput.add(company3);
+        Assert.assertEquals(gson.toJson(expectedOutput), gson.toJson(companies));
     }
 
     @Test
@@ -92,5 +105,25 @@ public class CompanyControllerTest {
             }
         });
         Assert.assertEquals(1, companies.size());
+    }
+
+    @Test
+    public void should_return_company_when_get_company_with_id() {
+        MockMvcResponse response = given()
+                .when()
+                .get(DEFAULT_PATH_PREFIX + "/companies/3");
+        Assert.assertEquals(200, response.getStatusCode());
+        String company = response.getBody().asString();
+        Assert.assertEquals(gson.toJson(company3), company);
+    }
+
+    @Test
+    public void should_return_employees_when_get_employees_in_company() {
+        MockMvcResponse response = given()
+                .when()
+                .get(DEFAULT_PATH_PREFIX + "/companies/3/employees");
+        Assert.assertEquals(200, response.getStatusCode());
+        String employee = response.getBody().asString();
+        Assert.assertEquals(gson.toJson(company3.getEmployees()), employee);
     }
 }
